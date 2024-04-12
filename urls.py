a8757d5220c212ed5963487b263f484970555bcb
Deepika srinivasan <146057466+DeepiSrinivasan@users.noreply.github.com>
2024-04-12 21:13:36 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,15 +1,22 @@
-from django.urls import path
-from . import views
+"""myproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path,include
+from django.contrib import admin
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('findbus', views.findbus, name="findbus"),
-    path('bookings', views.bookings, name="bookings"),
-    path('cancellings', views.cancellings, name="cancellings"),
-    path('seebookings', views.seebookings, name="seebookings"),
-    path('signup', views.signup, name="signup"),
-    path('signin', views.signin, name="signin"),
-    path('success', views.success, name="success"),
-    path('signout', views.signout, name="signout"),
-
+    path('admin/', admin.site.urls),
+    path('', include('myapp.urls')),
 ]
